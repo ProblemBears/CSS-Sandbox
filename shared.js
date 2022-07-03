ea2f60f99b730e61efbb505151e64704dc9f1d36
Jonathan Martinez <jonathan6md123@yahoo.com>
2022-07-03 16:40:58 -0400

    263 - CSS animations and JS eventHandlers relationship to them

--- shared.js
+++ shared.js
@@ -4,6 +4,7 @@ let buttons = document.querySelectorAll('.plan button');
 let noButton = document.querySelector(".modal__action--negative");
 let toggleButton = document.querySelector('.toggle-button');
 let mobileNav = document.querySelector('.mobile-nav');
+let ctaButton = document.querySelector('.main-nav__item--cta');
 
 for(let i = 0; i < buttons.length; i++)
 {
@@ -33,6 +34,18 @@ function closeModal()
 }
 
 toggleButton.addEventListener('click', () => {
-    mobileNav.style.display = 'block';
-    backdrop.style.display = 'block';
+    mobileNav.classList.add("open");
+    backdrop.classList.add("open");
+});
+
+ctaButton.addEventListener('animationstart', (e) => {
+    console.log('Animation Started', e);
+});
+
+ctaButton.addEventListener('animationend', (e) => {
+    console.log('Animation Ended', e);
+});
+
+ctaButton.addEventListener('animationiteration', (e) => {
+    console.log('Animation Iteration', e);
 });
